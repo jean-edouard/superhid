39b217cacca7fa0c653aa17b6edeabdc302ae6ce
Jed <lejosnej@ainfosec.com>
2015-09-16 18:39:37 -0400

    starting to handle setup requests using actual superhid code
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- src/main.c
+++ src/main.c
@@ -65,6 +65,8 @@
 #include <xenbackend.h>
 
 #include <linux/usb/ch9.h>
+#include <linux/hid.h>
+#include <xen/grant_table.h>
 
 #include "usbif.h"
 
@@ -72,6 +74,52 @@
  * The (stupid) logging macro
  */
 #define xd_log(I, ...) do { fprintf(stderr, ##__VA_ARGS__); fprintf(stderr, "\n"); } while (0)
+
+/* Report IDs for the various devices */
+#define REPORT_ID_KEYBOARD      0x01
+#define REPORT_ID_MOUSE         0x02
+#define REPORT_ID_TABLET        0x03
+#define REPORT_ID_MULTITOUCH    0x04
+#define REPORT_ID_STYLUS        0x05
+#define REPORT_ID_PUCK          0x06
+#define REPORT_ID_FINGER        0x07
+#define REPORT_ID_MT_MAX_COUNT  0x10
+#define REPORT_ID_CONFIG        0x11
+#define REPORT_ID_INVALID       0xff
+
+/* This is a relative mouse with 5 buttons and a vertical wheel. */
+#define MOUSE\
+  0x05, 0x01,                   /* USAGE_PAGE (Generic Desktop)     */ \
+    0x09, 0x02,                 /* USAGE (Mouse)                    */ \
+    0xa1, 0x01,                 /* COLLECTION (Application)         */ \
+    0x85, REPORT_ID_MOUSE,      /*   REPORT_ID (2)                  */ \
+    0x09, 0x01,                 /*   USAGE (Pointer)                */ \
+    0xa1, 0x00,                 /*   COLLECTION (Physical)          */ \
+    0x05, 0x09,                 /*     USAGE_PAGE (Button)          */ \
+    0x19, 0x01,                 /*     USAGE_MINIMUM (Button 1)     */ \
+    0x29, 0x05,                 /*     USAGE_MAXIMUM (Button 5)     */ \
+    0x15, 0x00,                 /*     LOGICAL_MINIMUM (0)          */ \
+    0x25, 0x01,                 /*     LOGICAL_MAXIMUM (1)          */ \
+    0x95, 0x05,                 /*     REPORT_COUNT (5)             */ \
+    0x75, 0x01,                 /*     REPORT_SIZE (1)              */ \
+    0x81, 0x02,                 /*     INPUT (Data,Var,Abs)         */ \
+    0x95, 0x01,                 /*     REPORT_COUNT (1)             */ \
+    0x75, 0x03,                 /*     REPORT_SIZE (3)              */ \
+    0x81, 0x03,                 /*     INPUT (Cnst,Var,Abs)         */ \
+    0x05, 0x01,                 /*     USAGE_PAGE (Generic Desktop) */ \
+    0x09, 0x30,                 /*     USAGE (X)                    */ \
+    0x09, 0x31,                 /*     USAGE (Y)                    */ \
+    0x09, 0x38,                 /*     USAGE (Z)                    */ \
+    0x15, 0x81,                 /*     LOGICAL_MINIMUM (-127)       */ \
+    0x25, 0x7f,                 /*     LOGICAL_MAXIMUM (127)        */ \
+    0x75, 0x08,                 /*     REPORT_SIZE (8)              */ \
+    0x95, 0x03,                 /*     REPORT_COUNT (3)             */ \
+    0x81, 0x06,                 /*     INPUT (Data,Var,Rel)         */ \
+    0x95, 0x01,                 /*     REPORT_COUNT (1)             */ \
+    0x75, 0x08,                 /*     REPORT_SIZE (8)              */ \
+    0x81, 0x03,                 /*     INPUT (Cnst,Var,Abs)         */ \
+    0xc0,                       /*   END_COLLECTION                 */ \
+    0xc0                        /* END_COLLECTION                   */
 
 typedef struct dominfo
 {
@@ -108,6 +156,7 @@ struct superhid_backend
   struct superhid_device *device;
 };
 
+static xc_gnttab *xcg_handle = NULL;
 static struct xs_handle *xs_handle;
 char *xs_dom0path = NULL;
 usbif_back_ring_t back_ring;
@@ -369,11 +418,194 @@ xenstore_create_usb(dominfo_t *domp, usbinfo_t *usbp)
   return -1;
 }
 
+
+
+struct hid_descriptor {
+  __u8  bLength;
+  __u8  bDescriptorType;
+  __le16 bcdHID;
+  __u8  bCountryCode;
+  __u8  bNumDescriptors;
+
+  /* struct hid_class_descriptor desc[1]; */
+} __attribute__ ((packed));
+
+static struct hid_descriptor hid_dt_desc = {
+  .bLength= sizeof(struct hid_descriptor),
+  .bDescriptorType= HID_DT_HID,
+  .bcdHID= 0x0101,
+  .bCountryCode= 0x00,
+  .bNumDescriptors= 0x1,
+};
+
+struct hid_desc {
+  unsigned char         subclass;
+  unsigned char         protocol;
+  unsigned short        report_length;
+  unsigned short        report_desc_length;
+  unsigned char         report_desc[];
+};
+
+static struct feature_report {
+  char feature;
+  char value;
+};
+
+struct hid_desc superhid_desc = {
+  .subclass= 0, /* No subclass */
+  .protocol= 0,
+  .report_length= 8,
+  .report_desc_length= 144,
+  /* Length without the mouse: */
+  /* .report_desc_length= 84, */
+  .report_desc= {
+    MOUSE,
+    0x05, 0x0D,         /*  Usage Page (Digitizer),             */
+    0x09, 0x04,         /*  Usage (Touchscreen),                */
+    0xA1, 0x01,         /*  Collection (Application),           */
+    0x85, REPORT_ID_MULTITOUCH, /* Report ID (4),               */
+    0x09, 0x22,         /*      Usage (Finger),                 */
+    0xA1, 0x00,         /*      Collection (Physical),          */
+    0x09, 0x42,         /*          Usage (Tip Switch),         */
+    0x15, 0x00,         /*          Logical Minimum (0),        */
+    0x25, 0x01,         /*          Logical Maximum (1),        */
+    0x75, 0x01,         /*          Report Size (1),            */
+    0x95, 0x01,         /*          Report Count (1),           */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x09, 0x32,         /*          Usage (In Range),           */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x09, 0x37,         /*          Usage (Data Valid),         */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x25, 0x1F,         /*          Logical Maximum (31),       */
+    0x75, 0x05,         /*          Report Size (5),            */
+    0x09, 0x51,         /*          Usage (51h),                */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x05, 0x01,         /*          Usage Page (Desktop),       */
+    0x55, 0x0E,         /*          Unit Exponent (14),         */
+    0x65, 0x11,         /*          Unit (Centimeter),          */
+    0x35, 0x00,         /*          Physical Minimum (0),       */
+    0x75, 0x10,         /*          Report Size (16),           */
+    0x46, 0x56, 0x0A,   /*          Physical Maximum (2646),    */
+    0x26, 0xFF, 0x0F,   /*          Logical Maximum (4095),     */
+    0x09, 0x30,         /*          Usage (X),                  */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x46, 0xB2, 0x05,   /*          Physical Maximum (1458),    */
+    0x26, 0xFF, 0x0F,   /*          Logical Maximum (4095),     */
+    0x09, 0x31,         /*          Usage (Y),                  */
+    0x81, 0x02,         /*          Input (Variable),           */
+    0x05, 0x0D,         /*          Usage Page (Digitizer),     */
+    0x75, 0x08,         /*          Report Size (8),            */
+    0x85, REPORT_ID_MT_MAX_COUNT, /* Report ID (10),            */
+    0x09, 0x55,         /*          Usage (55h),                */
+    0x25, 0x10,         /*          Logical Maximum (16),       */
+    0xB1, 0x02,         /*          Feature (Variable),         */
+    0xC0,               /*      End Collection,                 */
+    0xC0                /*  End Collection                      */
+  }
+};
+
+static int superhid_setup(struct usb_ctrlrequest *setup, void *buf)
+{
+  __u16 value, length;
+  struct feature_report feature;
+
+  value = setup->wValue;
+  length = setup->wLength;
+
+  switch ((setup->bRequestType << 8) | setup->bRequest) {
+  case ((USB_DIR_IN | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
+        | HID_REQ_GET_REPORT):
+    printf("get_report\n");
+
+    if ((value & 0xFF) == 0x10)
+    {
+      /* All right... We just received feature request 0x10 */
+      /* Right now there's no system for handling feature requests */
+      /* But Windows 8 needs an answer to that. */
+      /* It won't work without knowing how many fingers we handle. */
+      /* The device we "emulate" here is the Samsung Slate 7, which
+       * handles 8 fingers */
+      /* Let's reply that we do the same! */
+      feature.feature = 0x10;
+      feature.value = 0x08;
+      length = 2;
+      memcpy(buf, &feature, length);
+    }
+    else
+    {
+      /* send an empty report */
+      if (superhid_desc.report_length < length)
+        length = superhid_desc.report_length;
+      memset(buf, 0x0, length);
+    }
+
+    goto respond;
+    break;
+
+  case ((USB_DIR_IN | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
+        | HID_REQ_GET_PROTOCOL):
+    printf("get_protocol\n");
+    goto stall;
+    break;
+
+  case ((USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
+        | HID_REQ_SET_REPORT):
+    printf("set_report | wLenght=%d\n", setup->wLength);
+    goto stall;
+    break;
+
+  case ((USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
+        | HID_REQ_SET_PROTOCOL):
+    printf("set_protocol\n");
+    goto stall;
+    break;
+
+  case ((USB_DIR_IN | USB_TYPE_STANDARD | USB_RECIP_INTERFACE) << 8
+        | USB_REQ_GET_DESCRIPTOR):
+    switch (value >> 8) {
+    case HID_DT_HID:
+      printf("USB_REQ_GET_DESCRIPTOR: HID\n");
+      if (hid_dt_desc.bLength < length)
+        length = hid_dt_desc.bLength;
+      memcpy(buf, &hid_dt_desc, length);
+      goto respond;
+      break;
+    case HID_DT_REPORT:
+      printf("USB_REQ_GET_DESCRIPTOR: REPORT\n");
+      if (superhid_desc.report_desc_length < length)
+        length = superhid_desc.report_desc_length;
+      memcpy(buf, superhid_desc.report_desc, length);
+      goto respond;
+      break;
+
+    default:
+      printf("Unknown descriptor request 0x%x\n",
+           value >> 8);
+      goto stall;
+      break;
+    }
+    break;
+
+  default:
+    printf("Unknown request 0x%x\n",
+         setup->bRequest);
+    goto stall;
+    break;
+  }
+
+stall:
+  return 0;
+
+respond:
+  return length;
+}
+
 void consume_requests(void)
 {
   RING_IDX rc, rp;
   usbif_request_t req;
   usbif_response_t rsp;
+  int responded;
 
   if (back_ring_ready != 1) {
     printf("not ready to consume\n");
@@ -400,18 +632,46 @@ void consume_requests(void)
     printf("flags=%d\n", req.flags);
     printf("nr_packets=%d\n", req.nr_packets);
     printf("startframe=%d\n", req.startframe);
-    printf("***********************\n");
+    responded = 0;
     if (req.type == USBIF_T_GET_SPEED) {
       rsp.id            = req.id;
       rsp.actual_length = 0;
       rsp.data          = USB_SPEED_HIGH;
       rsp.status        = USBIF_RSP_OKAY;
+      responded = 1;
+    }
+    if (req.type == USBIF_T_RESET) {
+      rsp.id            = req.id;
+      rsp.actual_length = 0;
+      rsp.data          = 0;
+      rsp.status        = USBIF_RSP_OKAY;
+      responded = 1;
+    }
+    if (req.setup != 0) {
+      struct usb_ctrlrequest setup;
+      void *buf;
+
+      memcpy(&setup, &req.setup, sizeof(struct usb_ctrlrequest));
+      printf("SETUP.bRequestType=%X\n", setup.bRequestType);
+      printf("SETUP.bRequest=%X\n", setup.bRequest);
+      printf("SETUP.wValue=%X\n", setup.wValue);
+      printf("SETUP.wIndex=%X\n", setup.wIndex);
+      printf("SETUP.wLength=%X\n", setup.wLength);
+      printf("SETUP ");
+      buf = xc_gnttab_map_grant_ref(xcg_handle,
+                                    backend->domid,
+                                    req.u.gref[req.startframe],
+                                    PROT_READ | PROT_WRITE);
+      responded = superhid_setup(&setup, buf + req.offset);
+    }
+    if (responded != 0) {
       memcpy(RING_GET_RESPONSE(&back_ring, back_ring.rsp_prod_pvt), &rsp, sizeof(rsp));
       back_ring.rsp_prod_pvt++;
       RING_PUSH_RESPONSES(&back_ring);
       backend_evtchn_notify(backend->backend, backend->device->devid);
     }
     back_ring.req_cons++;
+    printf("***********************\n");
   }
 }
 
@@ -553,6 +813,13 @@ int main(int argc, char **argv)
     xd_log(LOG_ERR, "Failed to connect to xenstore");
     return 1;
   }
+  if (xcg_handle == NULL) {
+    xcg_handle = xc_gnttab_open(NULL, 0);
+  }
+  if (xcg_handle == NULL) {
+    xd_log(LOG_ERR, "Failed to connect to xc");
+    return 1;
+  }
   if (xs_dom0path == NULL) {
     xs_dom0path = xs_get_domain_path(xs_handle, 0);
   }
@@ -621,6 +888,7 @@ int main(int argc, char **argv)
 
   /* Cleanup */
   xs_daemon_close(xs_handle);
+  xc_gnttab_close(xcg_handle);
 
   return 0;
 }
