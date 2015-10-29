1d025b05fac4224f9f034df6f1928d179e75bc6d
Jed <lejosnej@ainfosec.com>
2015-10-29 16:41:44 -0400

    fix another printf
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- src/superbackend.c
+++ src/superbackend.c
@@ -94,7 +94,7 @@ void consume_requests(struct superhid_device *dev)
       superbackend_send(dev, &rsp);
       break;
     case USBIF_T_INT: /* Interrupt request. Pend it. */
-      xd_log(LOG_DEBUG, "%" PRIu64 ": pendings[%d]=%d", dev->devid, dev->pendingtail, req.id);
+      xd_log(LOG_DEBUG, "%d: pendings[%d]=%" PRIu64, dev->devid, dev->pendingtail, req.id);
       dev->pendings[dev->pendingtail] = req.id;
       dev->pendingrefs[dev->pendingtail] = req.u.gref[0];
       dev->pendingoffsets[dev->pendingtail] = req.offset;
